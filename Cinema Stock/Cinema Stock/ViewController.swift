d366beea78277c9ae9adc38698abdd2dc6ffb6d3
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2016-12-06 23:21:34 -0500

    Initial Project Files
    All files created at the beginning of an Xcode project.

---
+++ Cinema Stock/Cinema Stock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cinema Stock
+//
+//  Created by Morgan Colbert on 12/6/16.
+//  Copyright © 2016 Morgan Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
