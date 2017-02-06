9e284eec7146311d9cb3e786a79af39f38639006
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-02-06 14:00:11 -0500

    Connect UI to code
    Added outlets for the text fields and action methods for the buttons.
    Nothing happens with the UI yet but the skeleton framework for the
    buttons and fields are set up.

--- Cinema Stock/Cinema Stock/ViewController.swift
+++ Cinema Stock/Cinema Stock/ViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	//MARK: Properties
+	@IBOutlet weak var movieTitleTextField: UITextField!
+	@IBOutlet weak var releaseYearTextField: UITextField!
+	@IBOutlet weak var ratedTextField: UITextField!
+	@IBOutlet weak var actorsTextField: UITextField!
+	@IBOutlet weak var actressTextField: UITextField!
+	@IBOutlet weak var writersTextField: UITextField!
+	@IBOutlet weak var directorsTextField: UITextField!
+	@IBOutlet weak var movieDescriptionTextField: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
+	//MARK: Actions
+	@IBAction func addActorButton(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func addActressButton(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func addWriterButton(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func addDirectorButton(_ sender: UIButton) {
+		
+	}
 }
 
