edeb60dbb8a1c5173be0b70e44f92f8d66647b11
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-02-08 11:39:29 -0500

    More Code Connecting
    Started adding methods to each of the field elements. These methods add
    functionality that allows the fields to accept user input and do
    something with it. The methods are still just skeletons right now, but
    they are the basis for how each of the fields will behave.

--- Cinema Stock/Cinema Stock/ViewController.swift
+++ Cinema Stock/Cinema Stock/ViewController.swift
@@ -8,39 +8,156 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 	
 	//MARK: Properties
 	@IBOutlet weak var movieTitleTextField: UITextField!
 	@IBOutlet weak var releaseYearTextField: UITextField!
 	@IBOutlet weak var ratedTextField: UITextField!
+	
 	@IBOutlet weak var actorsTextField: UITextField!
+	@IBOutlet weak var actorsTextField2: UITextField!
+	@IBOutlet weak var actorsTextField3: UITextField!
+	
 	@IBOutlet weak var actressTextField: UITextField!
+	@IBOutlet weak var actressTextField2: UITextField!
+	@IBOutlet weak var actressTextField3: UITextField!
+	
 	@IBOutlet weak var writersTextField: UITextField!
+	@IBOutlet weak var writersTextField2: UITextField!
+	@IBOutlet weak var writersTextField3: UITextField!
+	
 	@IBOutlet weak var directorsTextField: UITextField!
+	@IBOutlet weak var directorsTextField2: UITextField!
+	@IBOutlet weak var directorsTextField3: UITextField!
+	
 	@IBOutlet weak var movieDescriptionTextField: UITextField!
 	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		// Handle the text field's input through delegate callbacks.
+		movieTitleTextField.delegate = self
+		releaseYearTextField.delegate = self
+		ratedTextField.delegate = self
+		actorsTextField.delegate = self
+		actorsTextField2.delegate = self
+		actorsTextField3.delegate = self
+		actressTextField.delegate = self
+		actressTextField2.delegate = self
+		actressTextField3.delegate = self
+		writersTextField.delegate = self
+		writersTextField2.delegate = self
+		writersTextField3.delegate = self
+		directorsTextField.delegate = self
+		directorsTextField2.delegate = self
+		directorsTextField3.delegate = self
+		movieDescriptionTextField.delegate = self
 	}
+	
+	//MARK: UITextFieldDelegate
+	
+	// Movie title
+	func movieTitleTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		movieTitleTextField.resignFirstResponder()
+		return true
+	}
+	
+	func movieTitleTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Release year
+	func releaseYearTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func releaseYearTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Rated
+	func ratedTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func ratedTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Actors
+	func actorsTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func actorsTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Actress
+	func actressTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func actressTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Writers
+	func writersTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func writersTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Directors
+	func directorsTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func directorsTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
+	// Movie description
+	func movieDescriptionTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func movieDescriptionTextFieldDidEndEditing(_ textField: UITextField) {
+		<#code#>
+	}
+	
 	
 	//MARK: Actions
+	// Actor button
 	@IBAction func addActorButton(_ sender: UIButton) {
-		
+		// unhide the text fields. probably need a simple for loop for this to only unhide one box at a time.
+		actorsTextField2.accessibilityElementsHidden = false
 	}
 	
+	// Actress button
 	@IBAction func addActressButton(_ sender: UIButton) {
-		
+		// unhide the text fields. probably need a simple for loop for this to only unhide one box at a time.
 	}
 	
+	// Writer button
 	@IBAction func addWriterButton(_ sender: UIButton) {
-		
+		// unhide the text fields. probably need a simple for loop for this to only unhide one box at a time.
 	}
 	
+	// Director button
 	@IBAction func addDirectorButton(_ sender: UIButton) {
-		
+		// unhide the text fields. probably need a simple for loop for this to only unhide one box at a time.
 	}
 }
 
