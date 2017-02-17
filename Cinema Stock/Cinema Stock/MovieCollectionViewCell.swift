31d6106e5f06a3df00ec1e52f5f5d131f4a58f2e
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-02-16 21:26:43 -0500

    Added Files
    Added files to implement the functionality for the Collection View.

---
+++ Cinema Stock/Cinema Stock/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  Cinema Stock
+//
+//  Created by Morgan Colbert on 2/16/17.
+//  Copyright © 2017 Morgan Bailey. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Cinema Stock/Cinema Stock/MovieCollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewFlowLayout.swift
+//  Cinema Stock
+//
+//  Created by Morgan Colbert on 2/16/17.
+//  Copyright © 2017 Morgan Bailey. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+}
