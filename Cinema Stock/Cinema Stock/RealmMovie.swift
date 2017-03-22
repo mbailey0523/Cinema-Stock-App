06ac289a4f259da3b10c9ff6c4507f9f6e2c97a2
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2017-03-22 11:41:33 -0400

    Realm Data Model
    Added a file shift the data model from just swift to the Realm model.

---
+++ Cinema Stock/Cinema Stock/RealmMovie.swift
@@ -0,0 +1,19 @@
+//
+//  RealmMovie.swift
+//  Cinema Stock
+//
+//  Created by Morgan Colbert on 3/22/17.
+//  Copyright © 2017 Morgan Bailey. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmMovie: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
